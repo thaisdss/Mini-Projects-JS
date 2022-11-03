ac7be4f2f07ba6231f7f68d166782a12bade44dc
Thaís Silva <thaisdss.silva@gmail.com>
2022-11-03 13:57:41 -0300

    update 006-calculator

--- 006-Calculator/calculator.js
+++ 006-Calculator/calculator.js
@@ -13,7 +13,7 @@ const PendingOperation = () => operator != undefined;
 const calculate = () => {
     console.log(operator)
     if(PendingOperation()){
-        const currentNumber = parseFloat(display.textContent);
+        const currentNumber = parseFloat(display.textContent.replace(",", "."));
         newNumber = true;
         const result = eval (`${previousNumber} ${operator} ${currentNumber}`);
         updateDisplay(result)
@@ -22,10 +22,10 @@ const calculate = () => {
 
 const updateDisplay = (text) => {
     if(newNumber){
-        display.textContent = text;
+        display.textContent = text.toLocaleString("BR");
         newNumber = false;
     }else{
-        display.textContent += text;
+        display.textContent += text.toLocaleString("BR");
     }
 }
 
@@ -37,13 +37,79 @@ const selectOperator = (event) => {
         calculate();
         newNumber = true;
         operator = event.target.textContent;
-        previousNumber = parseFloat(display.textContent);
+        previousNumber = parseFloat(display.textContent.replace(",", "."));
     }
-}
+};
 operators.forEach( operator => operator.addEventListener("click", selectOperator));
 
 const activeEqual = () => {
     calculate();
     operator = undefined;
+};
+document.getElementById("equal").addEventListener("click", activeEqual);
+
+const clearDisplay = () => display.textContent = "";
+document.getElementById("clearDisplay").addEventListener("click", clearDisplay);
+
+const clearCalculation = () => {
+    clearDisplay();
+    newNumber = true;
+    operator = undefined;
+    previousNumber = undefined
+};
+document.getElementById("clearCalculation").addEventListener("click", clearCalculation);
+
+const removeLastNumber = () => display.textContent = display.textContent.slice(0, -1)
+document.getElementById("backspace").addEventListener("click", removeLastNumber)
+
+const invertSignal = () => {
+    newNumber = true
+    updateDisplay(display.textContent * -1)
 }
-document.getElementById("equal").addEventListener("click", activeEqual());
+document.getElementById("invert").addEventListener("click", invertSignal);
+
+const thereIsDecimal = () => display.textContent.indexOf(",") != -1;
+const thereIsValue = () => display.textContent.length > 0;
+
+const insertDecimal = () => {
+    if(!thereIsDecimal()){
+        if(thereIsValue()){
+            updateDisplay(",")
+        }else{
+            updateDisplay("0,")
+        }
+    }
+};
+document.getElementById("decimal").addEventListener("click", insertDecimal);
+
+const keyboardMap = {
+    '0': 'key0',
+    '1': 'key1',
+    '2': 'key2',
+    '3': 'key3',
+    '4': 'key4',
+    '5': 'key5',
+    '6': 'key6',
+    '7': 'key7',
+    '8': 'key8',
+    '9': 'key9',
+    '/': 'operatorDivision',
+    '*': 'operatorMultiply',
+    '-': 'operatorSubtract',
+    '+': 'operatorAdd',
+    '=': 'equal',
+    'Enter': 'equal',
+    'Backspace': 'backspace',
+    'c': 'clearCalculation',
+    'Escape': 'clearDisplay',
+    ',': 'decimal'
+}
+
+const allowedKey = (key) => Object.keys(keyboardMap).indexOf(key) != -1;
+
+const mapKeyboard = (event) => {
+    const key = event.key;
+    
+    if(allowedKey(key)) document.getElementById(keyboardMap[key]).click();
+}
+document.addEventListener("keydown", mapKeyboard)
